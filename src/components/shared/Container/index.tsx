e6143169ad7dc82cdf1e5ed3be5e551477a76f8b
Kelson Eduardo <kelson.eduardo@codeminer42.com>
2025-04-07 15:46:36 -0300

    feat(ui): it creates the login page

---
+++ src/components/shared/Container/index.tsx
@@ -0,0 +1,8 @@
+function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      {children}
+    </div>
+  );
+}
+export { Container };
