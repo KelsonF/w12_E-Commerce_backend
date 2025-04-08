8a1391c08a3d879977615d52f9b70807e45339d2
Clara Maestri <clara.maestri@codeminer42.com>
2025-04-08 12:13:49 -0300

    refactor: changing img component to use nextJS image

--- src/components/shared/Img/index.tsx
+++ src/components/shared/Img/index.tsx
@@ -1,12 +1,22 @@
+import Image, { StaticImageData } from 'next/image';
+
 type ImgProps = {
-    placeholder?: string;
-  };
-  
-  function Img({ placeholder }: ImgProps) {
-    return (
-      <img src={placeholder} alt={placeholder} className="w-36 h-36 object-cover" />
-    );
-  }
-  
-  export { Img };
-  
+  source: string | StaticImageData;
+  placeholder?: string;
+  w?: number;
+  h?: number;
+};
+
+function Img({ source, placeholder, w, h}: ImgProps) {
+  return (
+    <Image
+      src={source}
+      alt={placeholder || 'Image'}
+      width={w}
+      height={h}
+      className="object-cover w-36 h-36"
+    />
+  );
+}
+
+export { Img };
--- src/pages/product-list/index.tsx
+++ src/pages/product-list/index.tsx
@@ -9,7 +9,7 @@ export default function ProductList() {
     <Container>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center">
         <Container>
-            <Img placeholder={placeholderimage.src}/>
+            <Img source={placeholderimage.src} w={144} h={144}/>
             <div className="flex flex-col items-left gap-1 justify-start">
             <p className="text-lg text-[#111827] font-[Inter] font-bold">Product Name</p>
             <p className="text-sm text-[#4B5563] font-[Inter] font-light">$ 0.00</p>
